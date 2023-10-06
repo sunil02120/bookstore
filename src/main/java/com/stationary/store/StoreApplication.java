6c63433064908c25afabccb754d601d4bad57463
sunil <sunil02120@gmail.com>
2023-10-06 14:36:58 +0530

    Code added

--- src/main/java/com/stationary/store/StoreApplication.java
+++ src/main/java/com/stationary/store/StoreApplication.java
@@ -2,12 +2,19 @@ package com.stationary.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StoreApplication {
+public class StoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StoreApplication.class);
 	}
 
 }
